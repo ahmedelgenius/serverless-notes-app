4b27c13b3eaab87f88e5c325214622f18407cefd
ahmedhany <elgenius10000@gmail.com>
2022-08-28 06:29:22 +0200

    Adding serverless error logging

--- services/util/debug.js
+++ services/util/debug.js
@@ -1,21 +1,16 @@
 import util from "util";
 import AWS from "aws-sdk";
-
 let logs;
-
 // Log AWS SDK calls
 AWS.config.logger = { log: debug };
-
 export default function debug() {
   logs.push({
     date: new Date(),
     string: util.format.apply(null, arguments),
   });
 }
-
 export function init(event) {
   logs = [];
-
   // Log API event
   debug("API event", {
     body: event.body,
@@ -23,7 +18,6 @@ export function init(event) {
     queryStringParameters: event.queryStringParameters,
   });
 }
-
 export function flush(e) {
   logs.forEach(({ date, string }) => console.debug(date, string));
   console.error(e);
--- services/util/handler.js
+++ services/util/handler.js
@@ -1,4 +1,4 @@
-// import * as debug from "./debug";
+import * as debug from "./debug";
 
 export default function handler(lambda) {
   return async function (event, context) {
